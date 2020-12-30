da53a4cbea0fcad98125629509cfb8c3d33be731
mthomp35 <mthomp35@users.noreply.github.com>
2020-12-29 22:01:38 -0800

    Add delete button "x"

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,15 @@ if (inputValue === '') {
    $('#list').append(li);
  }
 
- // Crossing out an item from the list of items
- li.on('dblclick', function() {
-   li.toggleClass('strike');
-  });
+// Crossing out an item from the list of items
+li.on('dblclick', function() {
+ li.toggleClass('strike');
+});
+
+// Adding the delete button "X"
+let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+li.append(crossOutButton);
+
+crossOutButton.on('click', function() {
+  li.addClass('delete');
+});
