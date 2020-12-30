0048e856db757de529e595c3ce9e0c2a42945d2b
mthomp35 <mthomp35@users.noreply.github.com>
2020-12-29 21:59:57 -0800

    Add a new item to the list

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Adding a new item to the list of items
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+   alert("You must write something!");
+ } else {
+   $('#list').append(li);
+ }
